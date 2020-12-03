90b1d514e7f19469aa129326de6eb9d83da1e384
tomatoaas <tomomi.314159265@gmail.com>
2020-12-03 13:35:19 +0900

    購入詳細表のmigrationを作成

---
+++ database/migrations/2020_12_02_031118_create_purchase_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchaseDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('shop_id')->unsigned();
+            $table->integer('price');
+            $table->integer('product_id')->unsigned();
+            $table->datetime('date');
+            $table->foreign('shop_id')->references('id')->on('shops');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchase_details');
+    }
+}
