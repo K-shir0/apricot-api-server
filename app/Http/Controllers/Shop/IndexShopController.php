54b4d279fc1388d59ab341662586f66f706a44b3
K-shir0 <kakishiro2190394@gmail.com>
2021-01-09 01:19:29 +0900

    feat: shop一覧を表示できるようにした

--- app/Http/Controllers/Shop/IndexShopController.php
+++ app/Http/Controllers/Shop/IndexShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Shop;
 
 use App\Http\Controllers\Controller;
+use App\Models\Shop;
 use Illuminate\Http\Request;
 
 class IndexShopController extends Controller
@@ -10,11 +11,11 @@ class IndexShopController extends Controller
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Http\Response
      */
     public function __invoke(Request $request)
     {
-        //
+        return Shop::query()->get();
     }
 }
