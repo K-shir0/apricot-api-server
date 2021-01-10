0d8eab84b58ba6484add901f53abd01b7b567c3e
K-shir0 <kakishiro2190394@gmail.com>
2021-01-10 23:25:29 +0900

    feat: カテゴリで絞り込む機能を追加

--- app/Http/Controllers/Product/IndexProductController.php
+++ app/Http/Controllers/Product/IndexProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\Controller;
+use App\Models\product;
 use Illuminate\Http\Request;
 
 class IndexProductController extends Controller
@@ -11,10 +12,22 @@ class IndexProductController extends Controller
      * Handle the incoming request.
      *
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function __invoke(Request $request)
     {
-        //
+        $category_id_list = $request->query('category_id');
+
+        // 商品一覧
+        $products = Product::query();
+
+        // カテゴリで絞り込む
+        if ($category_id_list) {
+            $products->whereIn('category_id', $category_id_list);
+        }
+
+        return response()->json(
+            $products->get()
+        );
     }
 }
