7e5b954676fd34ad21268878c7fa2cbc4a248350
K-shir0 <kakishiro2190394@gmail.com>
2021-01-19 13:17:37 +0900

    feat: 検索で表示するようにした

--- app/Http/Controllers/Usecase/CategoryOrProductSearchController.php
+++ app/Http/Controllers/Usecase/CategoryOrProductSearchController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Usecase;
 
+use App\Category;
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CategoryOrProductSearchController extends Controller
@@ -11,10 +13,22 @@ class CategoryOrProductSearchController extends Controller
      * Handle the incoming request.
      *
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function __invoke(Request $request)
     {
-        //
+        $searchWord = $request->query('q');
+
+        $products = Product::query();
+        $categories = Category::query();
+        if ($searchWord) {
+            $products->where('name', 'like', '%' . $searchWord . '%');
+            $categories->where('name', 'like', '%' . $searchWord . '%');
+        }
+
+        return response()->json([
+            'products' => $products->get(),
+            'categories' => $categories->get()
+        ]);
     }
 }
